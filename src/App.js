869cfaf667f1b3c243f70a0c09fda933ee6784c2
Farhan Laeeq <farhanali27718@gmail.com>
2022-12-01 16:02:43 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="basic">
+      <h1>Heading 1</h1>
+      <p>This is a paragrph.</p>
+    </div>
+    </>
+  );
+}
+
+export default App;
